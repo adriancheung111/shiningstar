c4b09e0a1b6fce14ebc4905b66e7cbe4e0c197e8
adriancheung111 <adrian04033@gmail.com>
2021-12-29 21:24:03 +0800

    index slider update

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $('.slicklist').slick({
     dots: true,
     arrows: true,
-    autoplay: true,
+    autoplay: false,
     adaptiveHeight: true,
     autoplaySpeed: 5000,
     responsive: [
