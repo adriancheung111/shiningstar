3d23de09d9f94cd237151eede0c2bc9bf3487bc9
adriancheung111 <adrian04033@gmail.com>
2022-01-13 00:39:08 +0800

    release 1.5
    fee

--- js/allcourse.js
+++ js/allcourse.js
@@ -1,18 +1,18 @@
-  var header = document.getElementById("testbtn");
-  var btns = header.getElementsByClassName("btn1");
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
-      var current = document.getElementsByClassName("active");
-      if (current.length > 0) {
-        current[0].className = current[0].className.replace(" active", "");
-      }
-      this.className += " active";
-    });
-  }
+var header = document.getElementById("testbtn");
+var btns = header.getElementsByClassName("btn1");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+  });
+}
 
-  function display(selected) {
-    if (selected == 'math1') {
-      texttoshow = `<h1>中學數學HKDSE課程</h1>
+function display(selected) {
+  if (selected == 'math1') {
+    texttoshow = `<h1>中學數學HKDSE課程</h1>
       (常規課程 每堂1小時15分鐘)<br><br>
       <ul>
       <li>依照學校進度量身安排，兼備概念和操練，為同學拔尖補底</li>
@@ -22,13 +22,11 @@
       </ul>
       <hr>
       <b>費用:<br>
-      小一至小三 $700/4堂<br>
-      小四至小六 $750/4堂 <br>
       中一至中三 $800/4堂<br>
       中四至中六 $850/4堂</b>`;
-    }
-    else if (selected == 'math2') {
-      texttoshow = `<h1>小學數學呈分試課程</h1>
+  }
+  else if (selected == 'math2') {
+    texttoshow = `<h1>小學數學呈分試課程</h1>
       (常規課程 每堂1小時15分鐘)<br><br>
       <ul>
       <li>由淺入深為同學講解升中重點課題，以呈分試為教學方向，與同學操練每課呈分試重點題目，入讀心宜band1中學。</li>
@@ -37,12 +35,10 @@
       <hr>
       <b>費用:<br>
       小一至小三 $700/4堂<br>
-      小四至小六 $750/4堂 <br>
-      中一至中三 $800/4堂<br>
-      中四至中六 $850/4堂</b>`;
-    }
-    else if (selected == 'english1') {
-      texttoshow = `<h1>中學英文HKDSE課程</h1>
+      小四至小六 $750/4堂</b>`;
+  }
+  else if (selected == 'english1') {
+    texttoshow = `<h1>中學英文HKDSE課程</h1>
       (常規課程 每堂1小時15分鐘)<br><br>
       <ul>
       <li>涵蓋「讀寫聽說」四個範疇，幫助同學打好文法基礎。實用獨家詳盡的筆記、練習、模擬試題、詞匯表等等，讓初中學生循序漸進地提昇英文水平，儘早為公開試作準備。</li>
@@ -51,13 +47,11 @@
       </ul>
       <hr>
       <b>費用:<br>
-      小一至小三 $700/4堂<br>
-      小四至小六 $750/4堂 <br>
       中一至中三 $800/4堂<br>
-      中四至中六 $850/4堂</b>`;
-    }
-    else if (selected == 'english2') {
-      texttoshow = `<h1>小學英文呈分試課程</h1>
+      中四至中六 $950/4堂</b>`;
+  }
+  else if (selected == 'english2') {
+    texttoshow = `<h1>小學英文呈分試課程</h1>
       (常規課程 每堂1小時15分鐘)<br><br>
       <ul>
       <li>獨家詳盡筆記、練習、詞匯表。</li>
@@ -65,13 +59,10 @@
       </ul>
       <hr>
       <b>費用:<br>
-      小一至小三 $700/4堂<br>
-      小四至小六 $750/4堂 <br>
-      中一至中三 $800/4堂<br>
-      中四至中六 $850/4堂</b>`;
-    }
-    else if (selected == 'other1') {
-      texttoshow = `<h1>升中3+1面試特訓班</h1>
+      小四至小六 $750/4堂</b>`;
+  }
+  else if (selected == 'other1') {
+    texttoshow = `<h1>升中3+1面試特訓班</h1>
       (精華短期課程 每堂1小時15分鐘)<br><br>
       <ul>
       <li>課程内容：教導學生如何用中文和英文進行面試，内容包括自我介紹、朗讀、問答和小組討論。</li>
@@ -80,6 +71,6 @@
       <hr>
       <b>費用:<br>
       $900 (3堂+1堂模擬面試)</b>`;
-    }
-    document.getElementById("thetext").innerHTML = texttoshow;
   }
+  document.getElementById("thetext").innerHTML = texttoshow;
+}
